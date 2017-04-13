98ee1ce9b764c2f60bd7f5f5e562529baa40e0e8
Andrew James <ajames20@gmail.com>
2017-04-13 10:30:59 +0300

    added state

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,14 +2,29 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  
+     goToStore(event) {
+      // 1. Grab text from input
+      // 2. Transition to from / to /store/:storeID
+      event.preventDefault();
+      const storeId =  this.storeInput.value;
+      this.context.router.transitionTo(`/store/:${storeId}`);
+    }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)} >
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input)=> {this.storeInput = input}}/>
         <button type="submit">Visit Store →</button>
       </form>
     );
   }
 }
+
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
+
 export default StorePicker;
