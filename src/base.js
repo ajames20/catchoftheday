701ec526cfc57e672812be22fdd313b6a476280d
Andrew James <ajames20@gmail.com>
2017-04-13 12:32:42 +0300

    Firebase added

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBRRabYyOLw-WhGEAZ8SfbiitZRiMCC9Uc",
+    authDomain: "catch-of-the-day-andrew-james.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-andrew-james.firebaseio.com",
+})
+
+export default base;
